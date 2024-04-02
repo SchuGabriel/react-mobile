868c7e037d750a02501d6426425c47241b4f69c3
Gabriel Schu <gabriel.101201@bento.local>
2024-04-02 20:22:05 -0300

    Ajustado para deletar na posica certa do array

--- componentes/Details.js
+++ componentes/Details.js
@@ -1,5 +1,5 @@
 import React from 'react';
-import { View, Text, Button } from 'react-native';
+import { View, Text, TouchableOpacity, StyleSheet } from 'react-native'; // Importando TouchableOpacity e StyleSheet
 import { useNavigation, useRoute } from '@react-navigation/native';
 
 function Details() {
@@ -9,23 +9,53 @@ function Details() {
     // Extrai o item passado como parâmetro de navegação
     const { item, data } = route.params;
 
-    console.log(item)
-    console.log(data)
-
     // Função para excluir o item
     const deleteItem = () => {
-        data.splice(item.id-1, 1)
+        let pos = null;
+        for (let i = 0; i < data.length; i++) {
+            if (item.name === data[i].name) {
+                pos = i;
+                break;
+            }
+        }
+        if (pos !== null) {
+            data.splice(pos, 1);
+        }
         navigation.goBack();
         return data;
     };
 
     return (
-        <View style={{ flex: 1, alignItems: 'center', justifyContent: 'center' }}>
-            <Text>{item.name}</Text>
-            <Text>{item.description}</Text>
-            <Button title="Delete Item" onPress={deleteItem} />
+        <View style={styles.container}>
+            <Text style={styles.text}>{item.name}</Text>
+            <Text style={styles.text}>{item.description}</Text>
+            <TouchableOpacity onPress={deleteItem} style={styles.button}>
+                <Text style={styles.buttonText}>Delete Item</Text>
+            </TouchableOpacity>
         </View>
     );
 }
+
+const styles = StyleSheet.create({
+    container: {
+        flex: 1,
+        alignItems: 'center',
+        justifyContent: 'center',
+    },
+    text: {
+        marginBottom: 10,
+    },
+    button: {
+        backgroundColor: 'red',
+        paddingVertical: 10,
+        paddingHorizontal: 20,
+        borderRadius: 5,
+    },
+    buttonText: {
+        color: 'white',
+        fontSize: 16,
+        fontWeight: 'bold',
+    },
+});
 
 export default Details;
